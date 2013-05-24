c29b65bed6dfb363e7a34522709dfac1f18b86f8
Ryan Westphal <ryan@trippingthebits.com>
2013-05-24 17:39:12 -0400

    first page tests

---
+++ spec/models/page_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe ( "Page model" ) {
+  let ( :page_one ) { FactoryGirl.create( :page_one ) }
+  let ( :page_two ) { FactoryGirl.create( :page_two ) }
+  let ( :page_three ) { FactoryGirl.create( :page_three ) }
+  let ( :page_four ) { FactoryGirl.create( :page_four ) }
+  let ( :page_five ) { FactoryGirl.create( :page_five ) }
+  let ( :page_six ) { FactoryGirl.create( :page_six ) }
+
+  describe ( "with valid data" ) {
+    subject { page_one }
+
+    it { should be_valid }
+    it { should respond_to( :work ) }
+    it { should respond_to( :image_group_image ) }
+    it { should respond_to( :image_url ) }
+  }
+
+  describe ( "with valid next page" ) {
+  }
+}
