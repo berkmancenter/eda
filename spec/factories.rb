813a5ca74e2eb9eee1a7ee7f73e022bc43b5ead1
Ryan Westphal <ryan@trippingthebits.com>
2013-05-24 12:34:39 -0400

    correct subfactories

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,54 +8,77 @@ FactoryGirl.define do
   end
 
   factory :work do
-    title "An altered look about the hills"
-    number 140
-
-    factory :work_with_edition do
-      edition
+    factory :work_one do
+      title "An altered look about the hills"
+      number 140
     end
 
+    edition
   end
 
-  factory :image_one do
-    url "http://zoom.it/oL3Y"
-    metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMDo1OTozOCAtMDQwMA==\n"
-    credits "Scanned by Harvard University"
+  factory :image do
+    factory :image_one do
+      url "http://zoom.it/oL3Y"
+      metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMDo1OTozOCAtMDQwMA==\n"
+      credits "Scanned by Harvard University"
+    end
+
+    factory :image_two do
+      url "http://zoom.it/aoUu"
+      metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMTowMDowNSAtMDQwMA==\n"
+      credits "Scanned by Harvard University"
+    end
+
+    factory :image_three do
+      url "http://zoom.it/c0nk"
+      metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMTowMDozOSAtMDQwMA==\n"
+      credits "Scanned by Harvard University"
+    end
+
+    factory :image_four do
+      url "http://zoom.it/WJnR"
+      metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMDo1OTo0OSAtMDQwMA==\n"
+      credits "Scanned by Harvard University"
+    end
+
+    factory :image_five do
+      url "http://zoom.it/2wzu"
+      metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMDo1OTo1MCAtMDQwMA==\n"
+      credits "Scanned by Harvard University"
+    end
   end
 
-  factory :image_two do
-    url "http://zoom.it/aoUu"
-    metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMTowMDowNSAtMDQwMA==\n"
-    credits "Scanned by Harvard University"
-  end
+  factory :image_group_image do
+    factory :igi_one do
+      image_one
+      position 1
+    end
 
-  factory :image_three do
-    url "http://zoom.it/c0nk"
-    metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMTowMDozOSAtMDQwMA==\n"
-    credits "Scanned by Harvard University"
-  end
+    factory :igi_two do
+      image_two
+      position 2
+    end
 
-  factory :image_four do
-    url "http://zoom.it/WJnR"
-    metadata "---\nImported: !binary |-\n  MjAxMy0wNS0yMyAxMDo1OTo0OSAtMDQwMA==\n"
-    credits "Scanned by Harvard University"
-  end
+    factory :igi_three do
+      image_three
+      position 3
+    end
 
-  factory :igi_one do
-    image_one
-    position 1
-  end
+    factory :igi_four do
+      image_three
+      position 1
+    end
 
-  factory :igi_two do
-    image_two
-    position 2
-  end
+    factory :igi_five do
+      image_four
+      position 2
+    end
 
-  factory :igi_three do
-    image_three
-    position 3
+    factory :igi_six do
+      image_five
+      position 1
+    end
   end
-
 end
 
 
--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe( "Work model" ) {
-  let ( :work ) { nil } 
+  let ( :work ) { FactoryGirl.create( :work_one ) }
 
   subject { work }
 
   describe( "with valid data" ) {
-    let ( :work ) { FactoryGirl.create( :work ) }
 
     it {
       should be_valid
@@ -14,12 +13,6 @@ describe( "Work model" ) {
   }
 
   describe( "with an edition" ) {
-    let ( :work ) { FactoryGirl.create( :work_with_edition ) }
-
-    it {
-      should be_valid
-    }
-
     describe( "the work's edition" ) {
       let ( :edition ) { work.edition }
 
