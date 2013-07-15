c53a73e133969ed2faccd5aca822462bd6cc2c58
Ryan Westphal <ryan@trippingthebits.com>
2013-07-15 14:25:19 -0400

    some basic search tests

---
+++ spec/requests/search_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe ( 'search requests' ) {
+  subject { page }
+
+  describe ( 'get /search' ) {
+    before { visit search_works_path }
+
+    it ( 'should return a search page' ) {
+      should have_selector( 'form[action="/search"]' );
+    }
+
+    it ( 'should not have a result list' ) {
+      should_not have_selector( '.search-results' );
+    }
+  }
+
+  describe ( 'get /search/awake (default edition)' ) {
+    # require test:seed
+    before { visit "#{search_works_path( 'awake' )}" } #?current_edition=3" }
+
+    it ( 'should return a search page' ) {
+      should have_selector( 'form[action="/search"]' );
+    }
+
+    it ( 'should have a result list' ) {
+      should have_selector( '.search-results' );
+    }
+
+    it ( 'should have one result' ) {
+      should have_css( '.search-results a', { count: 1 } );
+    }
+  }
+
+  describe ( 'get /search/awake?current_edition=1 ( no results in this edition )' ) {
+    # require test:seed
+    before { visit "#{search_works_path( 'awake' )}?current_edition=1" }
+
+    it ( 'should return a search page' ) {
+      should have_selector( 'form[action="/search"]' );
+    }
+
+    it ( 'should not have a result list' ) {
+      should have_selector( '.search-results' );
+    }
+
+    it ( 'should not have any results' ) {
+      should_not have_selector( '.search-results a' );
+    }
+  }
+}
