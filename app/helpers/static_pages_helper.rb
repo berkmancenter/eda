80fb5e607b5c8f614fc796d5986e9ce75778d0ed
Ryan Westphal <ryan@trippingthebits.com>
2013-08-05 16:56:44 -0400

    #5376 Icons for homepage

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def home_link( image, title, path )
+    link_to raw( "#{image_tag( image, { alt: title })} <span class=\"home-link-title\">#{title}</span>" ), path, { title: title }
+  end
 end
