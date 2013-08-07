f5476d463f30dd359ec537e3b9d304b1b581af2f
Ryan Westphal <ryan@trippingthebits.com>
2013-08-07 13:25:40 -0400

    use #page/x to open image_sets#show on a specific page, note that pages are 1-based

--- app/assets/javascripts/image_sets.js
+++ app/assets/javascripts/image_sets.js
@@ -2,9 +2,18 @@ $( function( ) {
   var sbsPages = $( ".sbs-pages" );
   
   if ( sbsPages.length ) {
+    var page;
+    if ( window.location.hash ) {
+      var hashParts = window.location.hash.split( '/' );
+      if ( $.isNumeric( hashParts[ 1 ] ) ) {
+        page = parseInt( hashParts[ 1 ] );
+      }
+
+    }
     sbsPages.turn( {
       width: 1000,
       height: 800,
+      page: page
     } );
   }
 } );
