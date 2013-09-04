28bc35b5e653049e656180d9a59c41d236332b9e
jdcc <justind.c.clark@gmail.com>
2013-09-04 11:21:41 -0400

    move mods around

--- lib/importers/transcriptions/franklin_ventura/importer.rb
+++ lib/importers/transcriptions/franklin_ventura/importer.rb
@@ -66,12 +66,34 @@ module FranklinVentura
             string = fix_font_changes(string)
             string.gsub!('<<', '&laquo;')
             string.gsub!('>>', '&raquo;')
-            string = sanitize(string, tags: %w(em b u p deviations fascicle line variant linenum year work title poem stanza publication manuscript number alternates emendations divisions revisions))
+            string.gsub!('<~>', '<br/>')
+            string = sanitize(string, tags: %w(br em b u p deviations fascicle line variant linenum year work title poem stanza publication manuscript number alternates emendations divisions revisions))
             string.sub!('</work>', '')
             string << "\n</work>\n"
             string.gsub!(/^(@|@1 = |@6.5PTS = |@PGBRK = |@PNT_1_1 = |@TRH1 = .*)$/, '')
             string.gsub!(/^\s*$\n/, '')
+            string = stick_modifiers_in_poems(string)
             string
+        end
+
+        def stick_modifiers_in_poems(string)
+            new_string = ''
+            have_poem_end = false
+            hold_pattern = /^(<divisions|<emendations|<revisions|<alternates)/
+            in_hold_pattern = false
+            poem_end_pattern = /<\/poem>/
+            string.each_line do |line|
+                if line.match(poem_end_pattern)
+                    have_poem_end = true
+                elsif in_hold_pattern && !line.match(hold_pattern) && have_poem_end
+                    new_string << "</poem>\n#{line}"
+                    have_poem_end = false
+                else
+                    new_string << line
+                end
+                in_hold_pattern = !!line.match(hold_pattern)
+            end
+            new_string
         end
 
         def markup_poem_lines(string)
@@ -140,122 +162,38 @@ module FranklinVentura
         end
 
         def process_file(file)
-            in_poem, ignore_next_line = false, false
-            poem, stanza = nil, nil
-            tags, variant_titles = [], []
-            held_holder_code, held_holder_subcode, held_holder_id = nil, nil, nil
-            file.each_line do |line|
-                # Find tags that we'll have to translate
-                #line.scan(/<([-0-9A-Z%]*)>/) { |m| tags << m[0] }
-                if ignore_next_line
-                    ignore_next_line = false
-                    next
-                end
-                if line.match(Ignore_next_line_pattern)
-                    ignore_next_line = true
-                end
 
-                # Is this a title?
-                if line.match(Title_pattern)
-                    match = Title_extractor.match(line)
-                    if match && Title_extractor.named_captures.keys.all?{ |name| match[name] }
-                        close_poem(poem) if poem
-                        poem = Work.new(
-                            number: match[:number].to_i,
-                            title: CharMap::replace_no_itals(match[:title]).strip,
-                            date: Date.new(File.basename(file.path).to_i)
-                        )
+            poem.holder_code = held_holder_code
+            poem.holder_subcode = held_holder_subcode
+            poem.holder_id = held_holder_id
+
+            if holder_match = line.match(Holder_extractor)
+                held_holder_code = holder_match[:loc_code].upcase.strip if holder_match[:loc_code]
+                held_holder_subcode = holder_match[:subloc_code].upcase.strip if holder_match[:subloc_code]
+                if holder_match[:subloc_code] == 'PC'
+                    held_holder_subcode = '1896PC'
+                end
+                held_holder_id = holder_match[:id].strip if holder_match[:id]
+            end
+
+            # Is this a second or third title?
+            match = Variant_title_extractor.match(line)
+            variant_titles << match[:title] if match && match[:title]
+
+            if line.match(Publication_pattern)
+                publications = line.match(Publication_extractor)['publications']
+                publications.scan(Published_extractor) do |match|
+                    args = Hash[Published_extractor.names.zip(match)]
+                    begin
+                        date = Date.parse([args['year'], args['month'], args['day']].join('-').gsub(/-*$/, ''))
+                    rescue
+                        date = nil
                     end
-                end
-
-                if held_holder_code && in_poem
-                    poem.holder_code = held_holder_code
-                    held_holder_code = nil
-                end
-                if held_holder_subcode && in_poem
-                    poem.holder_subcode = held_holder_subcode
-                    held_holder_subcode = nil
-                end
-                if held_holder_id && in_poem
-                    poem.holder_id = held_holder_id
-                    held_holder_id = nil
-                end
-
-                if holder_match = line.match(Holder_extractor)
-                    held_holder_code = holder_match[:loc_code].upcase.strip if holder_match[:loc_code]
-                    held_holder_subcode = holder_match[:subloc_code].upcase.strip if holder_match[:subloc_code]
-                    if holder_match[:subloc_code] == 'PC'
-                        held_holder_subcode = '1896PC'
-                    end
-                    held_holder_id = holder_match[:id].strip if holder_match[:id]
-                end
-
-                # Is this a second or third title?
-                match = Variant_title_extractor.match(line)
-                variant_titles << match[:title] if match && match[:title]
-
-                in_poem = true if line.match(Poem_start_pattern)
-
-                # Add all our line modifieres
-                add_modifiers!(poem, line)
-
-                if line.match(Publication_pattern)
-                    publications = line.match(Publication_extractor)['publications']
-                    publications.scan(Published_extractor) do |match|
-                        args = Hash[Published_extractor.names.zip(match)]
-                        begin
-                            date = Date.parse([args['year'], args['month'], args['day']].join('-').gsub(/-*$/, ''))
-                        rescue
-                            date = nil
-                        end
-                        poem.appearances << WorkAppearance.new(:year => args['year'].to_i, :date => date)
-                    end
-                end
-
-                if in_poem
-                    # Setup the stanza
-                    stanza = Stanza.new if line.match(Stanza_start_pattern)
-
-                    if line.match(Stanza_boundary_pattern)
-                        poem.stanzas << stanza
-                        stanza = Stanza.new
-                    end
-
-                    matches = Poem_line_extractors.map{ |e| e.match(line) } \
-                        .delete_if{|m| m.nil?} \
-                        .reduce({}){|captures, match| captures.merge(match.named_captures)}
-
-                    unless matches.empty?
-                        # If we have a new variant, create a new poem
-                        if matches['variant']
-                            if poem.variant.nil?
-                                poem.variant = CharMap::replace_no_itals(matches['variant'])
-                                puts "poem: #{poem.number} #{poem.variant}"
-                            else
-                                title = variant_titles.empty? ? poem.title : variant_titles.shift
-                                close_poem(poem)
-                                poem = Work.new(
-                                    number: poem.number,
-                                    title: CharMap::replace_no_itals(title).strip,
-                                    variant: CharMap::replace(matches['variant']),
-                                    date: Date.new(File.basename(file.path).to_i)
-                                )
-                                puts "poem: #{poem.number} #{poem.variant}"
-                            end
-                        end
-
-                        stanza_line = Line.new(:text => CharMap::replace(matches['line']).strip)
-                        stanza_line.number = line_number(poem, stanza, matches)
-                        stanza.lines << stanza_line
-                    end
-
-                    # Add stanza to poem if complete
-                    if line.match(Poem_end_pattern)
-                        poem.stanzas << stanza
-                        in_poem = false
-                    end
+                    poem.appearances << WorkAppearance.new(:year => args['year'].to_i, :date => date)
                 end
             end
+
+            title = variant_titles.empty? ? poem.title : variant_titles.shift
             close_poem(poem) if poem
         end
 
@@ -273,10 +211,10 @@ module FranklinVentura
             string = "<works>#{string}</works>"
 
             File.write(Rails.root.join('tmp', 'franklin_test.xml'), string)
-            works = parse_franklin(string)
-            edition.works = works
-            edition.save!
-            post_process!(edition)
+            #works = parse_franklin(string)
+            #edition.works = works
+            #edition.save!
+            #post_process!(edition)
         end
 
         def parse_franklin(string)
@@ -311,28 +249,25 @@ module FranklinVentura
                         end
                     end
                     w.save!
+                    add_modifiers!(w, work)
                     works << w
                 end
             end
             works
         end
 
-        def add_modifiers!(poem, line)
+        def add_modifiers!(poem, poem_xml)
             ['division', 'emendation', 'revision', 'alternate'].each do |var|
                 capped = var.camelize
-                next unless line.match("Patterns::#{capped}_pattern".constantize)
-                instances = prep_modifier(line, "Patterns::#{capped}_extractor".constantize, var.pluralize)
+                inner_xml = poem_xml.at(var.pluralize).inner_html
+                instances = prep_modifier(inner_xml)
+                puts instances.inspect
+                exit
                 instances.each do |instance|
                     i = self.send("get_#{var}", instance)
                     poem.line_modifiers.push(*i) if i 
                 end
             end
-        end
-
-        def close_poem(poem)
-            assign_stanza_positions(poem)
-            poem.save!
-            @poems << poem
         end
 
         def line_number(poem, stanza, matches)
@@ -355,56 +290,19 @@ module FranklinVentura
             locate_emendations!(edition)
             locate_divisions!(edition)
             locate_alternates!(edition)
-            fix_exceptions!(edition)
-            group_variants(edition)
         end
 
-        def fix_exceptions!(edition)
-            w = Work.find_by_number_and_variant(325, 'C')
-            w.stanzas.each_with_index{|s| s.destroy if s.position > 6} if w
-            w = Work.find_by_number_and_variant(321, 'C')
-            w.stanzas.each_with_index{|s| s.destroy if s.position > 0} if w
-        end
-
-        def group_variants(edition)
-            group = []
-            edition.works.each do |work|
-                if group.empty? || group.last.number == work.number
-                    group << work
-                elsif group.count > 1 && group.last.number != work.number
-                    wg = WorkSet.create!(:name => "#{group.last.number} variants")
-                    group.each do |w|
-                        ws = WorkSet.create!
-                        ws.work = w
-                        ws.move_to_child_of wg
-                        ws.save!
-                    end
-                    wg.save!
-                    group = [work]
-                else
-                    group = [work]
-                end
-            end
-        end
-
-        def prep_modifier(modifier, extractor, extracted)
-            mods = modifier.match(extractor)[extracted].split('<_><|><~>').drop(1) \
-                .map{|d| d.gsub("\r\n ",'').strip.split('<R>') }.flatten \
-                .delete_if do |d|
-                if d.include?('<MI>') 
+        def prep_modifier(inner_xml)
+            mods = inner_xml.split(/<br\s?\/>/).drop(1).delete_if do |d|
+                if d.include?('<em>') 
                     puts "mod: #{d}"
                     true
                 else
                     false 
                 end
-                end
-        end
-
-        def assign_stanza_positions(poem)
-            poem.stanzas.each_with_index do |s, i|
-                s.position = i
             end
         end
+
 
         def pattern(chars)
             Regexp.new("(^|\\b|\\s)#{Regexp.escape(chars)}($|\\b|\\s)")
