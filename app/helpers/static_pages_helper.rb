8e508168b3f9664e25852aac8d0142b11be2f148
Ryan Westphal <ryan@trippingthebits.com>
2013-09-23 18:44:08 -0400

    starting to put extra words on home links for #5747

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,5 +1,5 @@
 module StaticPagesHelper
-  def home_link( image, title, path )
-    link_to raw( "#{image_tag( image, { alt: title })} <span class=\"home-link-title\">#{title}</span>" ), path, { title: title }
+  def home_link( image, title, text, path )
+    link_to raw( "#{image_tag( image, { alt: title })} <span class=\"home-link-title\">#{title}</span><p>#{text}</p>" ), path, { title: title }
   end
 end
