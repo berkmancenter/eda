d396b0c8b0868c1b096f1f0b3c9323d6f2ad50b6
Justin Clark <jclark@cyber.law.harvard.edu>
2013-09-25 18:11:31 -0400

    start reordering images

--- lib/connectors/image_to_edition_connector.rb
+++ lib/connectors/image_to_edition_connector.rb
@@ -17,5 +17,25 @@ class ImageToEditionConnector
             edition.image_set.update_column(:rgt, Sett.maximum('rgt') + 1)
             edition.save!
         end
+
+        last_image = nil
+        root_image_set = ImageSet.new(name: 'All Images')
+
+        # TODO: Check that there are no blank pages in fascicles
+        # Use Houghton order for those not ordered by Franklin
+        works_by_fascicle = Work.all.group_by{|w| w.metadata['fascicle']}
+        works_by_fascicle.each do |fascicle, works|
+            image_set = ImageSet.new(name: "Fascicle #{fascicle}")
+            image_set.move_to_child_of root_image_set
+            works.sort_by!{|w| w.metadata['fascicle_order']}
+            works.each do |work|
+                work.image_set.each do |image|
+                    next if image == last_image
+                    image_set << image
+                    last_image = image
+                end
+            end
+        end
+        Work.all.group_by{|w| w.metadata['set']}
     end
 end
