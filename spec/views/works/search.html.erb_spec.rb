594e73bfb5eaead0b17da6f12aab4f1bc97d5fe0
Ryan Westphal <ryan@trippingthebits.com>
2013-09-26 17:55:33 -0400

    #5831 ui updates to works/table

---
+++ spec/views/works/search.html.erb_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe ( 'works/search' ) {
+  subject { rendered }
+
+  context ( 'normal work' ) {
+    before {
+      assign( :search, Work.search do
+          fulltext 'awake' do
+              fields(:lines, :title => 2.0)
+          end
+        end
+      )
+
+      render
+    }
+
+    it {
+      should have_css '.search-works'
+    }
+
+    it {
+      should have_css 'table.works'
+    }
+
+    it {
+      should have_css 'thead th', text: I18n.t( 'datatable.th_title' )
+      should have_css 'thead th', text: I18n.t( 'datatable.th_edition' )
+    }
+
+    it {
+      should have_css 'th:first-child', text: I18n.t( 'datatable.th_title' )
+    }
+  }
+}
