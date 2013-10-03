ce8fd98abe34152a0b73caeceed2df2f9dadc646
jdcc <justind.c.clark@gmail.com>
2013-10-02 22:34:21 -0400

    import sets and fasicles correctly

--- lib/connectors/image_to_edition_connector.rb
+++ lib/connectors/image_to_edition_connector.rb
@@ -26,8 +26,10 @@ class ImageToEditionConnector
        # TODO: Check that there are no blank pages in fascicles
        # Use Houghton order for those not ordered by Franklin
         works_by_fascicle = Work.all.group_by{|w| w.metadata['fascicle']}
+        fascicles = works_by_fascicle.keys.compact.sort
         pbar = ProgressBar.new("Connecting", works_by_fascicle.keys.count)
-        works_by_fascicle.each do |fascicle, works|
+        fascicles.each do |fascicle|
+            works = works_by_fascicle[fascicle]
             image_set = ImageSet.create(name: "Fascicle #{fascicle}")
             image_set.move_to_child_of root_image_set
             works.sort_by!{|w| w.metadata['fascicle_order']}
@@ -41,10 +43,28 @@ class ImageToEditionConnector
             end
             pbar.inc
         end
+
+        works_by_set = Work.all.group_by{|w| w.metadata['set']}
+        sets = works_by_set.keys.compact.sort
+        pbar = ProgressBar.new("Connecting", works_by_set.keys.count)
+        sets.each do |set|
+            works = works_by_set[set]
+            image_set = ImageSet.create(name: "Set #{set}")
+            image_set.move_to_child_of root_image_set
+            works.each do |work|
+                work.image_set.all_images.each do |image|
+                    # Don't add an image more than once (assumes in correct order)
+                    next if image == last_image
+                    image_set << image
+                    last_image = image
+                end
+            end
+            pbar.inc
+        end
+
         Edition.all.each do |edition|
             edition.image_set = root_image_set.duplicate
             edition.save!
         end
-        #works_by_set = Work.all.group_by{|w| w.metadata['set']}
     end
 end
