69ea061023933b5c47c198585cd4b20393c6e98e
jdcc <justind.c.clark@gmail.com>
2013-10-07 15:25:02 -0400

    updated lexicon

--- db/migrate/20130418135442_create_words.rb
+++ db/migrate/20130418135442_create_words.rb
@@ -2,9 +2,7 @@ class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
       t.string :word
-      t.string :endings
-      t.string :part_of_speech
-
+      t.string :sortable_word
       t.timestamps
     end
   end
