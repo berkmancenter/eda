c70f9d0f1ebd306e64bab62bb157ac9fe8e1d5e2
Ryan Westphal <ryan@trippingthebits.com>
2013-10-08 12:04:43 -0400

    accidentally committed a bad change

--- config/initializers/emily.rb
+++ config/initializers/emily.rb
@@ -1,4 +1,4 @@
 Eda::Application.config.emily = YAML.load_file(Rails.root.join('config', 'emily.yml'))[Rails.env]
 if ActiveRecord::Base.connection.table_exists? 'editions'
-    Eda::Application.config.emily['default_edition'] = Edition.find_by_name(Eda::Application.config.emily['default_edition'])
+    Eda::Application.config.emily['default_edition'] = Edition.find_by_short_name(Eda::Application.config.emily['default_edition'])
 end
