58477b0acb1edf2d7267d5ccd134e9f306702cc1
Ryan Westphal <ryan@trippingthebits.com>
2013-10-09 23:31:02 -0400

    #5099 faq links to images by name, not url

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,4 +2,13 @@ module StaticPagesHelper
   def home_link( image, title, text, path )
     link_to raw( "#{image_tag( image, { alt: title })} <span class=\"home-link-title\">#{title}</span><p>#{text}</p>" ), path, { title: title }
   end
+
+  def image_set_path_by_title( edition_prefix, image_title )
+	e = Edition.find_by_work_number_prefix( edition_prefix )
+	i = Image.find_by_title( image_title )
+
+	is = e.image_set.leaves_containing( i ) unless is.nil?
+
+	edition_image_set_path( e, is.first ) unless is.nil? || is.count == 0
+  end
 end
