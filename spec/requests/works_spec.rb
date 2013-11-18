953f83dd536580a2b43cb50e6694235e087f4b5b
Ryan Westphal <ryan@trippingthebits.com>
2013-11-18 17:16:17 -0500

    save browse table state + tests

--- spec/requests/works_spec.rb
+++ spec/requests/works_spec.rb
@@ -3,6 +3,64 @@ require 'spec_helper'
 describe 'works requests', :js => true do
   subject { page }
 
+  describe ( 'get /works' ) {
+    before { visit works_path }
+
+    it ( 'should work' ) {
+      should have_title 'Works'
+    }
+
+    it ( 'should have Edition column' ) {
+      should have_css 'th', text: 'Edition'
+    }
+
+    context ( 'with search within' ) {
+      before {
+        fill_in 'Search within these results:', with: 'sing'
+      }
+
+      it {
+        should have_css 'tbody tr', count: 2
+      }
+
+      describe ( 'click first link' ) {
+        before {
+          click_link 'Awake ye muses nine, sing me a strain divine'
+        }
+
+        it {
+          should have_title 'Manuscript View'
+        }
+
+        describe ( 'go back' ) {
+          before {
+            visit works_path
+          }
+
+          it {
+            should have_title 'Works'
+            should have_css '.dataTables_filter'
+          }
+
+          it {
+            filter = page.evaluate_script "$('.dataTables_filter input').val()"
+            puts filter
+            filter.should eq( 'sing' )
+          }
+
+        }
+      }
+    }
+  }
+
+  describe ( 'get /collections' ) {
+    before { visit collections_path }
+
+    it ( 'should work' ) {
+      should have_title 'Collections'
+    }
+  }
+
   describe ( 'get /editions/:edition_id/works' ) {
     before { visit edition_works_path Edition.find( 2 ) }
 
@@ -11,7 +69,7 @@ describe 'works requests', :js => true do
     }
 
     it ( 'should not have Edition column' ) {
-      should_not have_css 'td', text: 'Edition'
+      should_not have_css 'th', text: 'Edition'
     }
 
   }
