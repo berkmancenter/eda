cba5b4a21b7194166e90b7b88dddf128c88cecfe
Ryan Westphal <ryan@trippingthebits.com>
2013-11-21 17:23:43 -0500

    start building new notes partial

---
+++ spec/views/image_sets/_notes_list.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe ( 'image_sets/_notes_list' ) {
+  subject { rendered }
+
+  context ( 'normal notes' ) {
+    let ( :notes ) {
+      [ mock_model( Note ), mock_model( Note ) ]
+    }
+
+    before {
+      render partial: 'image_sets/notes_list', locals: { notes: notes }
+    }
+
+    it {
+      should have_css 'ul.notes'
+    }
+
+    it {
+      should have_css 'ul li.note', count: 2
+    }
+
+    it {
+      should have_css 'li span.note-text'
+    }
+
+    it {
+      should have_css 'li a.delete-note', text: 'x'
+    }
+  }
+}
