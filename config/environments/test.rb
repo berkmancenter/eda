0e323d3a39645de1dc11a3516e8fa72209544806
Ryan Westphal <ryan@trippingthebits.com>
2013-11-27 20:20:09 -0500

    set default host for test environment in case other modules (looking at you oai) call for _url named routes in our app

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,6 @@ Eda::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  Rails.application.routes.default_url_options[:host] = 'www.example.com'
 end
