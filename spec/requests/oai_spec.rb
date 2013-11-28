eeea32a51c14ffb7de70a015aad8109de7f62974
Ryan Westphal <ryan@trippingthebits.com>
2013-11-27 20:22:45 -0500

    first useful oai test (as request spec with xml parsing)

---
+++ spec/requests/oai_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe( 'oai requests' ) {
+  subject { page }
+
+  describe ( 'get /oai?verb-ListRecords&metadataPrefix=oai_dc' ) {
+    before { visit "#{oai_repository_path}?verb=ListRecords&metadataPrefix=oai_dc" }
+
+    it {
+      status_code.should eq( 200 )
+    }
+
+    it {
+      Hash.from_xml( source )[ 'OAI_PMH' ][ 'ListRecords' ].should_not eq( nil )
+    }
+  }
+}
