c77b2769a321cdddc40df4359c2c11d309648323
Justin Clark <jclark@cyber.law.harvard.edu>
2014-01-22 11:47:17 -0500

    closes #6715

--- app/models/work.rb
+++ app/models/work.rb
@@ -49,7 +49,7 @@ class Work < ActiveRecord::Base
 
     searchable do
         integer :edition_id
-        text(:number) { |work| work.number.to_s }
+        text(:number) { |work| "#{work.number} #{work.full_id} #{work.edition.work_number_prefix}#{work.number}" }
         text :title
         text :lines, stored: true do
             lines.map{|l| l.text }
