cb3fc7aa1b6d1cf906f1bfbdf3173c2a53a9b13b
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2020-03-25 16:30:04 +0100

    Use file storage for cache

--- app/helpers/file_cache_helper.rb
+++
@@ -1,8 +0,0 @@
-module FileCacheHelper
-  def with_file_cache
-    regular_cache = ActionController::Base.cache_store
-    ActionController::Base.cache_store = EDAFileCache
-    yield
-    ActionController::Base.cache_store = regular_cache
-  end
-end
--- config/initializers/file_cache.rb
+++ config/initializers/file_cache.rb
@@ -1,1 +1,2 @@
 EDAFileCache = ActiveSupport::Cache::FileStore.new(Rails.configuration.eda_file_cache)
+ActionController::Base.cache_store = EDAFileCache
