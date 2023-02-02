6995135ccecd9eb9ffe2defab55dbe325f958824
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-02-02 21:50:03 +0100

    Fix link on FAQ page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,14 +1,14 @@
 module StaticPagesHelper
-  def home_link( image, title, text, path )
-    link_to raw( "#{image_tag( image, { alt: title })} <span class=\"home-link-title\">#{title}</span><p>#{text}</p>" ), path, { title: title }
+  def home_link(image, title, text, path)
+    link_to raw("#{image_tag( image, { alt: title })} <span class=\"home-link-title\">#{title}</span><p>#{text}</p>"), path, { title: title }
   end
 
-  def image_set_path_by_title( edition_prefix, image_title )
-	e = Edition.where(work_number_prefix: edition_prefix)
-	i = Image.where(title: image_title).first
+  def image_set_path_by_title(edition_prefix, image_title)
+    e = Edition.where(work_number_prefix: edition_prefix).first
+    i = Image.where(title: image_title).first
 
-	is = e.image_set.leaves_containing( i ) unless i.nil?
+    is = e.image_set.leaves_containing(i) unless i.nil? && e.nil?
 
-	edition_image_set_path( e, is.first ) unless is.nil? || is.count == 0
+    edition_image_set_path( e, is.first ) unless is.nil? || is.count == 0
   end
 end
