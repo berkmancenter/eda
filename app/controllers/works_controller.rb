fffebb4c17d6b5b9d6310dce1f8b41382693518b
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-06-17 19:08:23 +0200

    Stop trying to load template in add_to_reading_list action

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -188,7 +188,11 @@ class WorksController < ApplicationController
     def add_to_reading_list
         @reading_list = ReadingList.find(params[:reading_list_id])
         @reading_list.add_work(@work)
-        render plain: !!@reading_list.save!
+        if @reading_list.save
+          head :ok
+        else
+          head :unprocessable_entity
+        end
     end
 
     private
